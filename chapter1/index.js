905efd228c85677c92f166504a474bfc74147352
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2021-08-04 13:38:09 +0200

    First Commit

---
+++ chapter1/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const port = 8000;
+app.listen(port, () => {
+  console.log("Server is listening on " + port);
+});
+var authors = [
+  {
+    name: "Lawrence Nowell",
+    nationality: "UK",
+    books: ["Beowulf"],
+  },
+  {
+    name: "William Shakespeare",
+    nationality: "UK",
+    books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"],
+  },
+  {
+    name: "Charles Dickens",
+    nationality: "US",
+    books: ["Oliver Twist", "A Christmas Carol"],
+  },
+  {
+    name: "Oscar Wilde",
+    nationality: "UK",
+    books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"],
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Authors API");
+});
