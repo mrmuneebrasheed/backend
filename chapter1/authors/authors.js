0ae280e3912a41c6b31fc60bace9c50fd5604f67
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2021-08-04 15:31:40 +0200

    Authors Exercises Done

--- chapter1/authors/authors.js
+++ chapter1/authors/authors.js
@@ -42,7 +42,26 @@ app.get("/authors/:id/books/", (req, res) => {
   const id = req.params.id;
   let books = "";
   for (let i = 0; i < authors[id - 1].books.length; i++) {
-    books.concat(authors[id - 1].books[i]);
+    books = books.concat(`${authors[id - 1].books[i]}, `);
   }
   res.send(`${books}`);
+});
+
+app.get("/json/authors/:id/", (req, res) => {
+  const id = req.params.id;
+  const idObject = {
+    name: authors[id - 1].name,
+    nationality: authors[id - 1].nationality,
+  };
+  const jsonString = JSON.stringify(idObject);
+  res.send(jsonString);
+});
+
+app.get("/json/authors/:id/books/", (req, res) => {
+  const id = req.params.id;
+  const idObject = {
+    books: authors[id - 1].books,
+  };
+  const jsonString = JSON.stringify(idObject);
+  res.send(jsonString);
 });
